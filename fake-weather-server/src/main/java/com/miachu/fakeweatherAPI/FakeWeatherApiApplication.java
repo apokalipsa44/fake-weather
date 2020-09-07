a19fcb486bb936594b335eede821bdce4b89d18a
apokalipsa44 <m_lachowicz@wp.pl>
2020-09-07 12:59:23 +0200

    initial commit

---
+++ fake-weather-server/src/main/java/com/miachu/fakeweatherAPI/FakeWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.miachu.fakeweatherAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeWeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeWeatherApiApplication.class, args);
+	}
+
+}
---
+++ fake-weather-server/src/test/java/com/miachu/fakeweatherAPI/FakeWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miachu.fakeweatherAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeWeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
